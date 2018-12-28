1b98772e4efae2878f74aa3356448f710a45ca86
klee <volibur@gmail.com>
2018-12-28 00:25:58 -0500

    Added new Trellee Controller and Trellee React Component.

--- Trellee/ClientApp/src/components/Trellee.js
+++ Trellee/ClientApp/src/components/Trellee.js
@@ -3,9 +3,45 @@
 export class Trellee extends Component {
   static displayName = Trellee.name;
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      loading: true
+    };
+
+    fetch('api/Trellee/ToDoCards')
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }
+
+  static renderCardsTable(cards) {
     return (
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>Cards</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cards.map(card => 
+            <td>{card.title}</td>  
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : Trellee.renderCardsTable(this.state.cards);
+
+    return (
+      <div>
         <h1>Welcome to Trellee!</h1>
+        {contents}
+      </div>
     );
   }
 }
---
+++ Trellee/Controllers/TrelleeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Trellee.Controllers
+{
+    [Route("api/[controller]")]
+    public class TrelleeController : Controller
+    {
+        private static Card[] Cards = new[]
+        {
+            new Card
+            {
+                Title = "Make an egg",
+                Description = "Crack open the egg first."
+            },
+
+            new Card
+            {
+                Title = "Read a book",
+                Description = "Read Foundation 2."
+            },
+
+            new Card
+            {
+                Title = "Practice Guitar",
+                Description = "Memorize the pentatonic scales."
+            }
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<Card> ToDoCards()
+        {
+            return Cards;
+        }
+
+        public class Card
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+    }
+
+
+}

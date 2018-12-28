ec3ccc50e52fa4af1c3279bfe866c3780bbfc8f1
klee <volibur@gmail.com>
2018-12-27 23:09:56 -0500

    initial commit

---
+++ Trellee/ClientApp/src/components/Trellee.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export class Trellee extends Component {
+  static displayName = Trellee.name;
+
+  render() {
+    return (
+        <h1>Welcome to Trellee!</h1>
+    );
+  }
+}
